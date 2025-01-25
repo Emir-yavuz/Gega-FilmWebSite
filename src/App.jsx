aca4bcfc6b61842189f940b53ca34d20333105e3
Emir-yavuz <emirsemihzehra@gmail.com>
2025-01-26 00:02:48 +0300

    a1

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div>
       {" "}
-      <h1 className="text-5xl font-bold underline">Hello world!</h1>
+      <h1 className="text-5xl font-bold underline">hi world!</h1>
     </div>
   );
 };
